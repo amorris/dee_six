e0d3d7a29f424fd09d52fee2693035d8b7a13ece
Andy <ajmorris@gmail.com>
2012-01-23 22:57:43 -0600

    cosmetic changes

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,6 @@ module DeeSix
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
